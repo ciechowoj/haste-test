65a77ce8013c61af37f794791d78e8efcb626818
ciechowoj <keepitsimplesirius@gmail.com>
2017-01-27 15:36:43 +0100

    Add range.

---
+++ range.cpp
@@ -0,0 +1,21 @@
+#include <haste/list>
+#include <haste/range>
+#include <haste/unittest>
+
+namespace haste {
+
+unittest() {
+  int result[16];
+  usize i = 0;
+
+  for (auto&& x : range<int>(0, 16)) {
+    result[i++] = x;
+  }
+
+  for (int i = 0; i < int(sizeof(result) / sizeof(result[0])); ++i) {
+    assert_eq(result[i], i);
+  }
+}
+
+unittest() { assert_eq(list<int>({0}), list<int>(range<int>(0, 1))); }
+}
